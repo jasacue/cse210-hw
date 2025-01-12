02b822d6220562c0dcc65150557fb631ad554244
jasacue <jasou21@proton.me>
2025-01-11 20:42:49 -0700

    prove1 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("what is your first name? ");
+        string1 = Console.Readline();
+        Console.WriteLine("What is your last name?");
+        String2 = Console.Readline();
+
+        Console.WriteLine($"Your name is {String2}, {string1} {String2}")
     }
 }
