db81fbcd684738ce671777ab922ec63294b5658d
jasacue <jasou21@proton.me>
2025-01-14 18:37:29 -0700

    Finished C# prep #5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string name = "";
+        int fav_num = 0;
+        int squared = 0;
+        DisplayMessage();
+        name = GetName(name);
+        fav_num = FavoriteNumber(fav_num);
+        squared = Square(fav_num);
+        DisplayResult(name, squared);
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program.");
+        }
+
+        static string GetName(string name)
+        {
+            Console.WriteLine("Please enter your name: ");
+            name = Console.ReadLine();
+            return name;
+        }
+        static int FavoriteNumber(int fav_num)
+        {
+            Console.WriteLine("What is your favorite number?");
+            return Int32.Parse(Console.ReadLine());
+        }
+        static int Square(int fav_num)
+        {
+            return fav_num*fav_num;
+        }
+        static void DisplayResult(string name, int squared)
+        {
+            Console.WriteLine($"{name}, your number squared is {squared}");
+        }
+
     }
 }
