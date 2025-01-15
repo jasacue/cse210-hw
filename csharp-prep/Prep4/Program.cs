ff63ba9d18ae6b9d06f165ba518d5c3e172fb59e
jasacue <jasou21@proton.me>
2025-01-14 18:15:36 -0700

    Finished C# prep #4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int entry = -1;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Please enter yor numbers: ");
+        while (entry != 0)
+        {
+            entry = Convert.ToInt32(Console.ReadLine());
+            numbers.Add(entry);
+        }
+        int total = 0;
+        foreach (int item in numbers)
+        {
+            total = total+item;
+            
+        }
+        Console.WriteLine($"total is {total}");
+        Console.WriteLine($"average is {total/(numbers.Count)}");
+        Console.WriteLine($"The largest number was {numbers.Max()}");
     }
 }
