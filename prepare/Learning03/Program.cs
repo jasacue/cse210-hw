714b9a15efe555543c95008789003e1bc1b693f8
Jasacue <Jasou21@proton.me>
2025-02-06 22:36:37 -0700

    Learning03 done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,6 +7,14 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractonString());
+        Console.WriteLine(f1.GetDecimalValue());
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractonString());
+        Console.WriteLine(f2.GetDecimalValue());
+        Fraction f3 = new Fraction(6, 7);
+        Console.WriteLine(f3.GetFractonString());
+        Console.WriteLine(f3.GetDecimalValue());
     }
 }
