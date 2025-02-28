05829299073f03992c86dddff743990b4dc6ed84
Jasacue <Jasou21@proton.me>
2025-02-27 18:04:58 -0700

    Fixed 03

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,44 @@
+class Reference
+{
+    private string _full;
+    private string _book;
+    private string _chapter;
+    private string _startVerse;
+    private string _endVerse;
+
+    public Reference(string book, string chapter, string startVerse, string endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = startVerse;
+        _endVerse = endVerse;
+        _condense();
+    }
+    public Reference(string book, string chapter, string startVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = startVerse;
+        _condense();
+    }
+    public void _condense()
+    {
+        string refer;
+        if (_startVerse == _endVerse)
+        {
+            refer = $"{_book} {_chapter}:{_startVerse}";
+        }
+        else
+        {
+            refer = $"{_book} {_chapter}:{_startVerse}-{_endVerse}";
+        }
+        
+        _full= refer;
+    }
+
+
+    public string getReference()
+    {
+        return $"{_full}";
+    }
+}
