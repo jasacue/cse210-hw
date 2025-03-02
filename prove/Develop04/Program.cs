9058930391a0ac068f8ffac9db2d41b0d79ccf64
Jasacue <Jasou21@proton.me>
2025-03-01 23:05:57 -0700

    Finished Mindfullness program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,42 @@
 using System;
-
+using System.ComponentModel;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        /*menu*/
+        string quit = "";
+        while (quit != "quit")
+        {
+            Console.WriteLine("Welcome to the Mindfullness Activity Launcher");
+            Console.WriteLine("1. Breathing Activity \n2. Reflection Activity \n3. Listing Activity \n4. Quit ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            switch (i)
+            {
+                case 1:
+                BreathingActivity breathe = new BreathingActivity("des", "name", "end", 0);
+                breathe.BreathActive();
+                break;
+
+                case 2:
+                ReflectionActivity reflect = new ReflectionActivity("", "", "", 0);
+                reflect.RefActive();
+                break;
+
+                case 3:
+                ListingActivity lis = new ListingActivity("", "", "", 0);
+                lis.ListActive();
+                break;
+
+                case 4:
+                Console.WriteLine("Goodbye");
+                quit = "quit";
+                break;
+            }
+        }
     }
 }
---
+++ prove/Develop04/ReflectionActivity.cs
@@ -0,0 +1,44 @@
+public class ReflectionActivity : Activity
+{
+    private List<string> _prompts = new List<string>();
+    private List<string> _questions = new List<string>();
+    public ReflectionActivity(string description, string name, string endDescription, int duration)
+        :base(description, name, endDescription, duration)
+        {
+            _description = "This activity will help you reflect on times in your life when you have shown strength and resilience. \nThis will help you recognize the power you have and how you can use it in other aspects of your life.";
+            _name = "Reflection Activity";
+            _prompts.Add("Think of a time when you stood up for someone else.");
+            _prompts.Add("Think of a time when you did something really difficult.");
+            _prompts.Add("Think of a time when you helped someone in need.");
+            _prompts.Add("Think of a time when you did something truly selfless.");
+            _questions.Add("Why was this experience meaningful to you?");
+            _questions.Add("Have you ever done anything like this before?");
+            _questions.Add("How did you get started?");
+            _questions.Add("How did you feel when it was complete?");
+            _questions.Add("What made this time different than other times when you were not as successful?");
+            _questions.Add("What is your favorite thing about this experience?");
+            _questions.Add("What could you learn from this experience that applies to other situations?");
+            _questions.Add("What did you learn about yourself through this experience?");
+            _questions.Add("How can you keep this experience in mind in the future?");
+            SetDuration();
+        }
+    public void RefActive()
+    {
+        StartActivity();
+        Random r = new Random();
+        int z = 0;
+        Thread.Sleep(2000);
+        while (z < _duration/8)
+        {
+            int i = r.Next(0, _prompts.Count());
+            Console.WriteLine($"{_prompts[i]} \n");
+            Spinner(2);
+            i = r.Next(0, _questions.Count());
+            Console.WriteLine($"{_questions[i]} \n");
+            Spinner(2);
+            z ++;
+        }
+        Console.WriteLine(_endMessage);
+    }
+
+}
