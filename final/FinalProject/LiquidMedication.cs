9a4fb5ea2b83cdcaa7ed63e1c0eb82485b8656e2
jasacue <jasou21@proton.me>
2025-03-31 13:13:47 -0600

    Fixed Errors

--- final/FinalProject/LiquidMedication.cs
+++ final/FinalProject/LiquidMedication.cs
@@ -4,7 +4,15 @@ public class LiquidMedication : Medication
 
 
 
-
+    public LiquidMedication(string name, string purpose, int dosage, string dosageUnit, string flavor)
+        :base (name, purpose, dosage, dosageUnit)
+    {
+        _name = name;
+        _purpose = purpose;
+        _dosage = dosage;
+        _dosageUnit = dosageUnit;
+        _flavor = flavor;
+    }
 
 
 }
